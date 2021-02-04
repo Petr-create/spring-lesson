95a5fc68fd96b75292a1d989185902830c6b89b9
Petr Dankin <petr2768@yandex.ru>
2021-02-04 18:58:19 +0300

    исправил названия методов иизменил метод в main (run)

--- src/main/java/ru/itsjava/annotations/myjunit/Main.java
+++ src/main/java/ru/itsjava/annotations/myjunit/Main.java
@@ -3,6 +3,6 @@ package ru.itsjava.annotations.myjunit;
 public class Main {
     public static void main(String[] args) {
         RunnerAnnotation runnerAnnotation =  new RunnerAnnotation();
-        runnerAnnotation.Before();
+        runnerAnnotation.run();
     }
 }
